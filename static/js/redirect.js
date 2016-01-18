7a3984d3b2a88e2146a1dd03e5f6f18f5589fc0f
Matan Shlomi <matanshlomi88@gmail.com>
2016-01-18 21:05:23 +0200

    update file redirect.js
    add the function that checks the uesr location

--- static/js/redirect.js
+++ static/js/redirect.js
@@ -3,3 +3,60 @@ function redirect(url)
 {
 	window.open(url, "_self");
 };
+
+
+/*
+function checkLocation(latitude, longitude)
+{
+	var x = document.getElementById("demo");
+	function getLocation() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition);
+		} else {
+			x.innerHTML = "Geolocation is not supported by this browser.";
+		}
+	}
+	function showPosition(position) {
+		
+		x.innerHTML = "Latitude: " + position.coords.latitude + 
+		"<br>Longitude: " + position.coords.longitude; 
+			
+		if(latitude>31 && latitude<32)
+			window.open("/commitmentWall/templates/commitment.html", "_self"); 
+		else
+			window.open("https://www.google.co.il/", "_self"); 
+		
+	}
+} 
+*/
+
+
+
+
+
+var x = document.getElementById("demo");
+var url;
+function checkLocation(u)
+{
+	url = u;
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else {
+		x.innerHTML = "Geolocation is not supported by this browser.";
+	}
+};
+
+
+function showPosition(position) 
+{
+	alert("Latitude: " + position.coords.latitude + "\n" +
+	"Longitude: " + position.coords.longitude);
+/* position.coords.latitude>31 && position.coords.latitude<32 */
+	if(position.coords.latitude>31 && position.coords.latitude<32)
+	{
+		redirect(url); 
+	}
+	else {
+		redirect("/commitmentWall/templates/locationError.html"); 
+	}
+};
